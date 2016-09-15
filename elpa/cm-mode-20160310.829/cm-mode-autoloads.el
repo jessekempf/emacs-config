3dd0f964503a4b0df189e54511853b88b74c0d27
Jesse Kempf <jessekempf@gmail.com>
2016-09-14 21:40:14 -0700

    add elpa

---
+++ elpa/cm-mode-20160310.829/cm-mode-autoloads.el
@@ -0,0 +1,15 @@
+;;; cm-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("cm-mode.el") (22430 24105 198240 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cm-mode-autoloads.el ends here
---
+++ elpa/rust-mode-20160517.346/rust-mode-autoloads.el
@@ -0,0 +1,15 @@
+;;; rust-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("rust-mode.el") (22430 34984 174995 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; rust-mode-autoloads.el ends here
